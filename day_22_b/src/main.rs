a2a1a0eb797867f68f1f942e40fbadb3a4cc007b
Torkel <torkel@voysys.se>
2021-12-31 13:47:33 +0100

    wip

--- day_22_b/src/main.rs
+++ day_22_b/src/main.rs
@@ -1,190 +1,147 @@
-use std::{collections::BTreeSet, error::Error, fs};
+use std::{collections::HashSet, error::Error, fs};
 
-fn split(
-    mut cubes: BTreeSet<((i64, i64), (i64, i64), (i64, i64))>,
-    splits: (Vec<i64>, Vec<i64>, Vec<i64>),
-) -> BTreeSet<((i64, i64), (i64, i64), (i64, i64))> {
-    {
-        let mut did_split = true;
-        while did_split {
-            did_split = false;
+#[derive(Hash, Eq, PartialEq, Clone, Copy)]
+struct Dim {
+    min: i32,
+    max: i32,
+}
 
-            let mut to_remove = BTreeSet::new();
-            let mut to_add = BTreeSet::new();
+#[derive(Hash, Eq, PartialEq, Clone, Copy)]
+struct Cube {
+    d: [Dim; 3],
+}
 
-            'cube_match_x: for cube in &cubes {
-                for split in &splits.0 {
-                    if cube.0 .0 < *split && cube.0 .1 >= *split {
-                        to_add.insert(((cube.0 .0, *split - 1), cube.1, cube.2));
-                        to_add.insert(((*split, cube.0 .1), cube.1, cube.2));
-                        to_remove.insert(*cube);
-                        did_split = true;
-                        continue 'cube_match_x;
-                    }
-                }
-            }
-
-            cubes.append(&mut to_add);
-            for r in to_remove {
-                cubes.remove(&r);
-            }
+fn overlaps(a: Cube, b: Cube) -> bool {
+    for d in 0..3 {
+        if (a.d[d].max > b.d[d].min && a.d[d].max < b.d[d].max)
+            || (a.d[d].min > b.d[d].min && a.d[d].min < b.d[d].max)
+            || (b.d[d].max > a.d[d].min && b.d[d].max < a.d[d].max)
+            || (b.d[d].min > a.d[d].min && b.d[d].min < a.d[d].max)
+        {
+            return true;
         }
     }
+    false
+}
 
-    {
+fn split(mut cubes: HashSet<Cube>, splits: [Vec<i32>; 3]) -> HashSet<Cube> {
+    for i in 0..3 {
         let mut did_split = true;
         while did_split {
             did_split = false;
 
-            let mut to_remove = BTreeSet::new();
-            let mut to_add = BTreeSet::new();
+            let mut to_remove = HashSet::new();
+            let mut to_add = HashSet::new();
 
-            'cube_match_y: for cube in &cubes {
-                for split in &splits.1 {
-                    if cube.1 .0 < *split && cube.1 .1 >= *split {
-                        to_add.insert((cube.0, (cube.1 .0, *split - 1), cube.2));
-                        to_add.insert((cube.0, (*split, cube.1 .1), cube.2));
+            'cube_match: for cube in &cubes {
+                for split in &splits[i] {
+                    if cube.d[i].min < *split && cube.d[i].max > *split {
+                        let mut new_cube_1 = cube.clone();
+                        new_cube_1.d[i].max = *split;
+
+                        let mut new_cube_2 = cube.clone();
+                        new_cube_2.d[i].min = *split;
+
+                        to_add.insert(new_cube_1);
+                        to_add.insert(new_cube_2);
                         to_remove.insert(*cube);
                         did_split = true;
-                        continue 'cube_match_y;
+                        continue 'cube_match;
                     }
                 }
             }
 
-            cubes.append(&mut to_add);
+            cubes.extend(to_add);
             for r in to_remove {
                 cubes.remove(&r);
             }
         }
-    }
-
-    {
-        let mut iters = 0;
-        let mut adds = 0;
-        let mut removes = 0;
-
-        let mut did_split = true;
-        while did_split {
-            did_split = false;
-
-            let mut to_remove = BTreeSet::new();
-            let mut to_add = BTreeSet::new();
-
-            'cube_match_z: for cube in &cubes {
-                for split in &splits.2 {
-                    if cube.2 .0 < *split && cube.2 .1 >= *split {
-                        to_add.insert((cube.0, cube.1, (cube.2 .0, *split - 1)));
-                        to_add.insert((cube.0, cube.1, (*split, cube.2 .1)));
-                        to_remove.insert(*cube);
-                        did_split = true;
-                        continue 'cube_match_z;
-                    }
-                }
-            }
-
-            adds += to_add.len();
-            removes += to_remove.len();
-
-            cubes.append(&mut to_add);
-            for r in to_remove {
-                cubes.remove(&r);
-            }
-
-            iters += 1;
-        }
-        println!("z: iters {}, adds: {}, removes: {}", iters, adds, removes);
     }
 
     cubes
 }
 
-fn split_points(
-    cubes: BTreeSet<((i64, i64), (i64, i64), (i64, i64))>,
-) -> (Vec<i64>, Vec<i64>, Vec<i64>) {
-    let mut x = Vec::new();
-    let mut y = Vec::new();
-    let mut z = Vec::new();
-
+fn split_points(cubes: HashSet<Cube>) -> [Vec<i32>; 3] {
+    let mut res = [Vec::new(), Vec::new(), Vec::new()];
     for cube in cubes {
-        x.push(cube.0 .0);
-        x.push(cube.0 .1 + 1);
-        y.push(cube.1 .0);
-        y.push(cube.1 .1 + 1);
-        z.push(cube.2 .0);
-        z.push(cube.2 .1 + 1);
+        for i in 0..3 {
+            res[i].push(cube.d[i].min);
+            res[i].push(cube.d[i].max);
+        }
     }
 
-    (x, y, z)
+    res
 }
 
-fn join(
-    mut cubes: BTreeSet<((i64, i64), (i64, i64), (i64, i64))>,
-) -> BTreeSet<((i64, i64), (i64, i64), (i64, i64))> {
+fn join(mut cubes: HashSet<Cube>) -> HashSet<Cube> {
     let mut did_join = true;
 
     while did_join {
         did_join = false;
 
-        let mut to_remove = BTreeSet::new();
-        let mut to_add = BTreeSet::new();
+        let mut to_remove = HashSet::new();
+        let mut to_add = HashSet::new();
 
-        let cube_vec: Vec<((i64, i64), (i64, i64), (i64, i64))> =
-            cubes.clone().into_iter().collect();
+        let cube_vec: Vec<Cube> = cubes.clone().into_iter().collect();
 
         if cube_vec.len() > 0 {
             'outer: for i1 in 0..cube_vec.len() - 1 {
                 for i2 in i1 + 1..cube_vec.len() {
                     let c1 = &cube_vec[i1];
                     let c2 = &cube_vec[i2];
-                    if c1.0 .1 + 1 == c2.0 .0 && c1.1 == c2.1 && c1.2 == c2.2 {
-                        to_remove.insert(*c1);
-                        to_remove.insert(*c2);
-                        to_add.insert(((c1.0 .0, c2.0 .1), c1.1, c1.2));
-                        did_join = true;
-                        break 'outer;
-                    }
-                    if c1.0 .0 == c2.0 .1 + 1 && c1.1 == c2.1 && c1.2 == c2.2 {
-                        to_remove.insert(*c1);
-                        to_remove.insert(*c2);
-                        to_add.insert(((c2.0 .0, c1.0 .1), c1.1, c1.2));
-                        did_join = true;
-                        break 'outer;
-                    }
 
-                    if c1.1 .1 + 1 == c2.1 .0 && c1.0 == c2.0 && c1.2 == c2.2 {
-                        to_remove.insert(*c1);
-                        to_remove.insert(*c2);
-                        to_add.insert((c1.0, (c1.1 .0, c2.1 .1), c1.2));
-                        did_join = true;
-                        break 'outer;
-                    }
-                    if c1.1 .0 == c2.1 .1 + 1 && c1.0 == c2.0 && c1.2 == c2.2 {
-                        to_remove.insert(*c1);
-                        to_remove.insert(*c2);
-                        to_add.insert((c1.0, (c2.1 .0, c1.1 .1), c1.2));
-                        did_join = true;
-                        break 'outer;
-                    }
+                    for i in 0..3 {
+                        {
+                            let mut can_extend = true;
+                            if c1.d[i].max != c2.d[i].min {
+                                can_extend = false;
+                            }
+                            for ii in 0..3 {
+                                if i != ii && c1.d[ii] != c2.d[ii] {
+                                    can_extend = false;
+                                }
+                            }
+                            if can_extend {
+                                to_remove.insert(*c1);
+                                to_remove.insert(*c2);
 
-                    if c1.2 .1 + 1 == c2.2 .0 && c1.1 == c2.1 && c1.0 == c2.0 {
-                        to_remove.insert(*c1);
-                        to_remove.insert(*c2);
-                        to_add.insert((c1.0, c1.1, (c1.2 .0, c2.2 .1)));
-                        did_join = true;
-                        break 'outer;
-                    }
-                    if c1.2 .0 == c2.2 .1 + 1 && c1.1 == c2.1 && c1.0 == c2.0 {
-                        to_remove.insert(*c1);
-                        to_remove.insert(*c2);
-                        to_add.insert((c1.0, c1.1, (c2.2 .0, c1.2 .1)));
-                        did_join = true;
-                        break 'outer;
+                                let mut new_cube = c1.clone();
+                                new_cube.d[i].max = c2.d[i].max;
+
+                                to_add.insert(new_cube);
+                                did_join = true;
+                                break 'outer;
+                            }
+                        }
+
+                        {
+                            let mut can_extend = true;
+                            if c2.d[i].max != c1.d[i].min {
+                                can_extend = false;
+                            }
+                            for ii in 0..3 {
+                                if i != ii && c1.d[ii] != c2.d[ii] {
+                                    can_extend = false;
+                                }
+                            }
+                            if can_extend {
+                                to_remove.insert(*c1);
+                                to_remove.insert(*c2);
+
+                                let mut new_cube = c2.clone();
+                                new_cube.d[i].max = c1.d[i].max;
+
+                                to_add.insert(new_cube);
+                                did_join = true;
+                                break 'outer;
+                            }
+                        }
                     }
                 }
             }
         }
 
-        cubes.append(&mut to_add);
+        cubes.extend(to_add);
         for r in to_remove {
             cubes.remove(&r);
         }
@@ -194,7 +151,7 @@ fn join(
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut cubes: BTreeSet<((i64, i64), (i64, i64), (i64, i64))> = BTreeSet::new();
+    let mut cubes: HashSet<Cube> = HashSet::new();
 
     let input = fs::read_to_string("input_test")?;
 
@@ -203,7 +160,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let line_count = lines.len();
 
     for line in lines.iter().enumerate() {
-        println!("line {}/{}: {}", line.0, line_count, line.1);
+        println!("line {}/{}: {}", line.0 + 1, line_count, line.1);
 
         let parts: Vec<&str> = line.1.split(" ").collect();
 
@@ -212,45 +169,79 @@ fn main() -> Result<(), Box<dyn Error>> {
         let mut ranges = Vec::new();
         for rr in raw_ranges {
             let parts: Vec<&str> = rr.split("=").collect();
-            let range: Vec<i64> = parts[1]
+            let range: Vec<i32> = parts[1]
                 .split("..")
-                .map(|s| s.parse::<i64>().unwrap())
+                .map(|s| s.parse::<i32>().unwrap())
                 .collect();
             ranges.push((range[0], range[1]));
         }
 
-        let fresh_one = (
-            (ranges[0].0, ranges[0].1),
-            (ranges[1].0, ranges[1].1),
-            (ranges[2].0, ranges[2].1),
-        );
+        let fresh_one = Cube {
+            d: [
+                Dim {
+                    min: ranges[0].0,
+                    max: ranges[0].1 + 1,
+                },
+                Dim {
+                    min: ranges[1].0,
+                    max: ranges[1].1 + 1,
+                },
+                Dim {
+                    min: ranges[2].0,
+                    max: ranges[2].1 + 1,
+                },
+            ],
+        };
 
-        let mut fresh: BTreeSet<((i64, i64), (i64, i64), (i64, i64))> = BTreeSet::new();
+        let mut fresh: HashSet<Cube> = HashSet::new();
         fresh.insert(fresh_one);
 
-        cubes = split(cubes, split_points(fresh.clone()));
+        let count_cubes1 = cubes.len();
 
-        fresh = split(fresh, split_points(cubes.clone()));
+        let mut affected = cubes.clone();
+        affected.retain(|v| overlaps(*v, fresh_one));
+        for a in &affected {
+            cubes.remove(a);
+        }
 
-        cubes = split(cubes, split_points(fresh.clone()));
+        let count_affected1 = affected.len();
 
-        fresh = split(fresh, split_points(cubes.clone()));
+        affected = split(affected, split_points(fresh.clone()));
+
+        fresh = split(fresh, split_points(affected.clone()));
+
+        affected = split(affected, split_points(fresh.clone()));
+
+        fresh = split(fresh, split_points(affected.clone()));
+
+        let count_affected2 = affected.len();
 
         if parts[0] == "on" {
-            cubes.extend(fresh);
+            affected.extend(fresh);
         } else {
             for f in fresh {
                 // println!("removing: {:?}", f);
-                cubes.remove(&f);
+                affected.remove(&f);
             }
         }
 
-        //  println!("cubes after: {:?}", cubes);
+        cubes.extend(affected);
+
+        let count_cubes2 = cubes.len();
+
+        println!(
+            "cubes1: {}, affected1: {}, cubes2: {}, affected2: {}",
+            count_cubes1, count_affected1, count_cubes2, count_affected2
+        );
 
         let mut volume: i64 = 0;
 
         for v in &cubes {
-            volume += (v.0 .1 - v.0 .0 + 1) * (v.1 .1 - v.1 .0 + 1) * (v.2 .1 - v.2 .0 + 1);
+            let mut cv = 1;
+            for i in 0..3 {
+                cv *= v.d[i].max as i64 - v.d[i].min as i64;
+            }
+            volume += cv;
         }
 
         println!("cube count: {}, volume: {:?}", cubes.len(), volume);
